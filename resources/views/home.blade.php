4877d222e26516a24ca752616755dbe0b2da58a2
大川峻 <ookawashun@ookawashunnoMacBook-Pro.local>
2020-05-13 10:57:58 +0900

    アップロードした画像を表示できる様にした

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<!-- エラーメッセージ。なければ表示しない -->
+@if ($errors->any())
+<ul>
+    @foreach($errors->all() as $error)
+    <li>{{ $error }}</li>
+    @endforeach
+</ul>
+@endif
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<!-- フォーム -->
+<form action="{{ url('upload') }}" method="POST" enctype="multipart/form-data">
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+    <!-- アップロードした画像。なければ表示しない -->
+    @isset ($filename)
+    <div>
+        <img src="{{ asset('storage/' . $filename) }}">
     </div>
-</div>
+    @endisset
+
+    <label for="photo">画像ファイル:</label>
+    <input type="file" class="form-control" name="file">
+    <br>
+    <hr>
+    {{ csrf_field() }}
+    <button class="btn btn-success"> Upload </button>
+</form>
 @endsection
