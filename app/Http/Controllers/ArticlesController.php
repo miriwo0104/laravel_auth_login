b11cdbc03f77169d33de3618272dcaa6c46fe5a9
大川峻 <ookawashun@ookawashunnoMacBook-Pro.local>
2020-05-20 12:48:01 +0900

    下記の記事の内容の実施を完了した
    https://qiita.com/sayama0402/items/668f7148759910f65da9

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Article;
+
 class ArticlesController extends Controller
 {
     /**
@@ -13,7 +15,9 @@ class ArticlesController extends Controller
      */
     public function index()
     {
-        //
+        // $articles変数にArticleモデルから全てのレコードを取得して、代入
+        $articles = Article::all();
+        return view('articles.index', ['articles' => $articles]);
     }
 
     /**
@@ -24,6 +28,7 @@ class ArticlesController extends Controller
     public function create()
     {
         //
+        return view('articles.create');
     }
 
     /**
@@ -34,8 +39,16 @@ class ArticlesController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        // モデルからインスタンスを生成
+        $article = new Article;
+        // $requestにformからのデータが格納されているので、以下のようにそれぞれ代入する
+        $article->title = $request->title;
+        $article->body = $request->body;
+        // 保存
+        $article->save();
+        // 保存後 一覧ページへリダイレクト
+        return redirect('/articles');
+    }  
 
     /**
      * Display the specified resource.
@@ -45,7 +58,10 @@ class ArticlesController extends Controller
      */
     public function show($id)
     {
-        //
+        // 引数で受け取った$idを元にfindでレコードを取得
+        $article = Article::find($id);
+        // viewにデータを渡す
+        return view('articles.show', ['article' => $article]);
     }
 
     /**
@@ -56,7 +72,8 @@ class ArticlesController extends Controller
      */
     public function edit($id)
     {
-        //
+        $article = Article::find($id);
+        return view('articles.edit', ['article' => $article]);
     }
 
     /**
@@ -68,7 +85,15 @@ class ArticlesController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        // idを元にレコードを検索して$articleに代入
+        $article = Article::find($id);
+        // editで編集されたデータを$articleにそれぞれ代入する
+        $article->title = $request->title;
+        $article->body = $request->body;
+        // 保存
+        $article->save();
+        // 詳細ページへリダイレクト
+        return redirect("/articles/".$id);
     }
 
     /**
@@ -80,5 +105,11 @@ class ArticlesController extends Controller
     public function destroy($id)
     {
         //
+        // idを元にレコードを検索
+        $article = Article::find($id);
+        // 削除
+        $article->delete();
+        // 一覧にリダイレクト
+        return redirect('/articles');
     }
 }
